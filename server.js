9952279606c182c55554812adcbd18f62076a6cc
KIRUBAHARAN8878M <kirubaharan8878m@gmail.com>
2022-10-01 19:29:46 +0530

    new repo

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const mongodb = require('mongodb');
 const dotenv = require('dotenv').config();
 const mongoClient =mongodb.MongoClient;
 const URL = process.env.DB;
-const DB = "Zen_Class"
+const DB = "Mongo"
 
 // let users = [];
+
 
 //middleware
 app.use(express.json());
@@ -15,22 +16,11 @@ app.use(cors({
    orgin: 'http://localhost:3000'
 }))
 
-app.get("/users", async function (req, res) {
-   // let qParms = req.query;
-   // let reUser = [];
-   // for (let index = parseInt(req.query.offset); index < parseInt(req.query.offset) + parseInt(req.query.limit); index++) {
-   //    if (users[index]) {
-   //       reUser.push(users[index]);
-   //    }
-
-   // }
-   // res.json(reUser);
-
-
+app.get("/getAllPizzas", async function (req, res) {
    try{    
    const connection = await mongoClient.connect(URL)  
    const db = connection.db(DB)   
-   let reUser = await db.collection("users").find().toArray();   
+   let reUser = await db.collection("pizzas").find().toArray();   
    await connection.close();
    res.json(reUser);
    
@@ -40,7 +30,7 @@ app.get("/users", async function (req, res) {
    }
 })
 
-app.post("/user", async function (req, res) {
+app.post("/addpizza", async function (req, res) {
    
 try{
    // step 1: Create a connection between NodeJS and MongoDB
@@ -49,7 +39,7 @@ const connection = await mongoClient.connect(URL)
 const db = connection.db(DB)
    // step 3: Select the Collection
    // step 4: Do the Operation(Create,Update,Edit,Delete)
-await db.collection("users").insertOne(req.body)
+await db.collection("pizzas").insertOne(req.body)
    // step 5: Close the Connection
 await connection.close()
 
@@ -59,22 +49,10 @@ res.json({message:"Data inserted"});
    res.status(500).json({message:"something went wrong"});
 
 }
-// if any error throw error 
 
-   // req.body.id = users.length + 1;
-   // users.push(req.body);
-   // res.json({ message: "User Created Successfully...." });
 })
 
-app.get("/user/:id",async function (req, res) {
-   // let userId = req.params.id;
-   // let user = users.find((item) => item.id == userId);
-   // if (user) {
-   //    res.json(user);
-   // } else {
-   //    res.json({ message: "User not found" });
-   // }
-
+app.get("/getpizzabyid/:id",async function (req, res) {
    try{
       // step 1: Create a connection between NodeJS and MongoDB
    const connection = await mongoClient.connect(URL)
@@ -82,7 +60,7 @@ app.get("/user/:id",async function (req, res) {
    const db = connection.db(DB)
       // step 3: Select the Collection
       // step 4: Do the Operation(Create,Update,Edit,Delete)
-   let user = await db.collection("users").findOne({_id : mongodb.ObjectId(req.params.id)})
+   let user = await db.collection("pizzas").findOne({_id : mongodb.ObjectId(req.params.id)})
       // step 5: Close the Connection
    await connection.close()
    
@@ -96,19 +74,8 @@ app.get("/user/:id",async function (req, res) {
 
 })
 
-app.put("/user/:id", async function (req, res) {
-   // let userId = req.params.id;
-   // let userIndex = users.findIndex((item) => item.id == userId);
-   // if (userIndex != -1) {
-   //    Object.keys(req.body).forEach((item) => {
-   //       users[userIndex][item] = req.body[item];
-   //    });
-   //    res.json({
-   //       message: "Edited Done"
-   //    });
-   // } else {
-   //    res.json({ message: "User not found" })
-   // }
+app.put("/updatedpizza/:id", async function (req, res) {
+
 
    try{
       // step 1: Create a connection between NodeJS and MongoDB
@@ -117,7 +84,7 @@ app.put("/user/:id", async function (req, res) {
    const db = connection.db(DB)
       // step 3: Select the Collection
       // step 4: Do the Operation(Create,Update,Edit,Delete)
-   let user = await db.collection("users").findOneAndUpdate({_id : mongodb.ObjectId(req.params.id)},{$set:req.body})
+   let user = await db.collection("pizzas").findOneAndUpdate({_id : mongodb.ObjectId(req.params.id)},{$set:req.body})
       // step 5: Close the Connection
    await connection.close()
    
@@ -132,17 +99,8 @@ app.put("/user/:id", async function (req, res) {
 
 })
 
-app.delete("/user/:id", async function (req, res) {
-   // let userId = req.params.id;
-   // let userIndex = users.findIndex((item) => item.id == userId);
-   // if (userIndex != -1) {
-   //    users.splice(userIndex, 1);
-   //    res.json({
-   //       message: "User Deleted Done"
-   //    });
-   // } else {
-   //    res.json({ message: "User not found" })
-   // }
+app.delete("/deletepizza/:id", async function (req, res) {
+
 
    try{
       // step 1: Create a connection between NodeJS and MongoDB
@@ -151,7 +109,7 @@ app.delete("/user/:id", async function (req, res) {
    const db = connection.db(DB)
       // step 3: Select the Collection
       // step 4: Do the Operation(Create,Update,Edit,Delete)
-   let user = await db.collection("users").findOneAndDelete({_id : mongodb.ObjectId(req.params.id)})
+   let user = await db.collection("pizzas").findOneAndDelete({_id : mongodb.ObjectId(req.params.id)})
       // step 5: Close the Connection
    await connection.close()
    
