5b6aa532b0a7f4bcc58095d3a28a62140fd8e7e2
KIRUBAHARAN8878M <kirubaharan8878m@gmail.com>
2022-10-01 23:08:22 +0530

    new

--- server.js
+++ server.js
@@ -1,182 +1,158 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongodb = require('mongodb');
-const bcrypt = require('bcryptjs');
-const dotenv = require('dotenv').config();
-const mongoClient =mongodb.MongoClient;
+const mongodb = require("mongodb");
+const bcrypt = require("bcryptjs");
+const dotenv = require("dotenv").config();
+const mongoClient = mongodb.MongoClient;
 const URL = process.env.DB;
-const DB = "Mongo"
+const DB = "Mongo";
 
 // let users = [];
 
-
 //middleware
 app.use(express.json());
-app.use(cors({
-   orgin: 'http://localhost:3000'
-}))
+app.use(
+  cors({
+    orgin: "http://localhost:3000",
+  })
+);
 
 app.get("/getAllPizzas", async function (req, res) {
-   try{    
-   const connection = await mongoClient.connect(URL)  
-   const db = connection.db(DB)   
-   let reUser = await db.collection("pizzas").find().toArray();   
-   await connection.close();
-   res.json(reUser);
-   
-   } catch(error){
-      res.status(500).json({message:"something went wrong"});
-   
-   }
-})
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
+    let reUser = await db.collection("pizzas").find().toArray();
+    await connection.close();
+    res.json(reUser);
+  } catch (error) {
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
 
 app.post("/addpizza", async function (req, res) {
-   
-try{
-   // step 1: Create a connection between NodeJS and MongoDB
-const connection = await mongoClient.connect(URL)
-   // step 2: Select the DB
-const db = connection.db(DB)
-   // step 3: Select the Collection
-   // step 4: Do the Operation(Create,Update,Edit,Delete)
-await db.collection("pizzas").insertOne(req.body)
-   // step 5: Close the Connection
-await connection.close()
+  try {
+    // step 1: Create a connection between NodeJS and MongoDB
+    const connection = await mongoClient.connect(URL);
+    // step 2: Select the DB
+    const db = connection.db(DB);
+    // step 3: Select the Collection
+    // step 4: Do the Operation(Create,Update,Edit,Delete)
+    await db.collection("pizzas").insertOne(req.body);
+    // step 5: Close the Connection
+    await connection.close();
 
-res.json({message:"Data inserted"});
+    res.json({ message: "Data inserted" });
+  } catch (error) {
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
 
-} catch(error){
-   res.status(500).json({message:"something went wrong"});
+app.get("/getpizzabyid/:id", async function (req, res) {
+  try {
+    // step 1: Create a connection between NodeJS and MongoDB
+    const connection = await mongoClient.connect(URL);
+    // step 2: Select the DB
+    const db = connection.db(DB);
+    // step 3: Select the Collection
+    // step 4: Do the Operation(Create,Update,Edit,Delete)
+    let user = await db
+      .collection("pizzas")
+      .findOne({ _id: mongodb.ObjectId(req.params.id) });
+    // step 5: Close the Connection
+    await connection.close();
 
-}
-
-})
-
-app.get("/getpizzabyid/:id",async function (req, res) {
-   try{
-      // step 1: Create a connection between NodeJS and MongoDB
-   const connection = await mongoClient.connect(URL)
-      // step 2: Select the DB
-   const db = connection.db(DB)
-      // step 3: Select the Collection
-      // step 4: Do the Operation(Create,Update,Edit,Delete)
-   let user = await db.collection("pizzas").findOne({_id : mongodb.ObjectId(req.params.id)})
-      // step 5: Close the Connection
-   await connection.close()
-   
-   res.json(user);
-   
-   } catch(error){
-      res.status(500).json({message:"something went wrong"});
-   
-   }
-
-
-})
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
 
 app.put("/updatedpizza/:id", async function (req, res) {
+  try {
+    // step 1: Create a connection between NodeJS and MongoDB
+    const connection = await mongoClient.connect(URL);
+    // step 2: Select the DB
+    const db = connection.db(DB);
+    // step 3: Select the Collection
+    // step 4: Do the Operation(Create,Update,Edit,Delete)
+    let user = await db
+      .collection("pizzas")
+      .findOneAndUpdate(
+        { _id: mongodb.ObjectId(req.params.id) },
+        { $set: req.body }
+      );
+    // step 5: Close the Connection
+    await connection.close();
 
-
-   try{
-      // step 1: Create a connection between NodeJS and MongoDB
-   const connection = await mongoClient.connect(URL)
-      // step 2: Select the DB
-   const db = connection.db(DB)
-      // step 3: Select the Collection
-      // step 4: Do the Operation(Create,Update,Edit,Delete)
-   let user = await db.collection("pizzas").findOneAndUpdate({_id : mongodb.ObjectId(req.params.id)},{$set:req.body})
-      // step 5: Close the Connection
-   await connection.close()
-   
-   res.json(user);
-   
-   } catch(error){
-      res.status(500).json({message:"something went wrong"});
-   
-   }
-})
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
 
 app.delete("/deletepizza/:id", async function (req, res) {
+  try {
+    // step 1: Create a connection between NodeJS and MongoDB
+    const connection = await mongoClient.connect(URL);
+    // step 2: Select the DB
+    const db = connection.db(DB);
+    // step 3: Select the Collection
+    // step 4: Do the Operation(Create,Update,Edit,Delete)
+    let user = await db
+      .collection("pizzas")
+      .findOneAndDelete({ _id: mongodb.ObjectId(req.params.id) });
+    // step 5: Close the Connection
+    await connection.close();
 
-
-   try{
-      // step 1: Create a connection between NodeJS and MongoDB
-   const connection = await mongoClient.connect(URL)
-      // step 2: Select the DB
-   const db = connection.db(DB)
-      // step 3: Select the Collection
-      // step 4: Do the Operation(Create,Update,Edit,Delete)
-   let user = await db.collection("pizzas").findOneAndDelete({_id : mongodb.ObjectId(req.params.id)})
-      // step 5: Close the Connection
-   await connection.close()
-   
-   res.json(user);
-   
-   } catch(error){
-      res.status(500).json({message:"something went wrong"});
-   
-   }
-
-
-})
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
 //user collection
 
+app.post("/register", async function (req, res) {
+  try {
+    const connection = await mongoClient.connect(URL);
+    const db = connection.db(DB);
 
- 
- app.post("/register", async function (req, res) {
-   
-   try{
-   const connection = await mongoClient.connect(URL)
-   const db = connection.db(DB)
-    
+    let salt = await bcrypt.genSalt(10);
+    let hash = await bcrypt.hash(req.body.password, salt);
 
-let salt = await bcrypt.genSalt(10);
-let hash = await bcrypt.hash(req.body.password, salt)
+    req.body.password = hash;
+    await db.collection("User").insertOne(req.body);
 
-req.body.password =hash;
-   await db.collection("User").insertOne(req.body)
-     
-   await connection.close()
-   
-   res.json({message:"Data inserted"});
-   
-   } catch(error){
-      res.status(500).json({message:"something went wrong"});
-   
-   }
-   
-   })
+    await connection.close();
 
-
-
- app.post("/login", async function (req, res) {
-  
-   try{
-      
-   const connection = await mongoClient.connect(URL)   
-   const db = connection.db(DB)
-      
-   let user = await db.collection("User").findOne({email : req.body.email});
-  if(user){
-console.log(req.body.password);
-let compare = await bcrypt.compare(req.body.password , user.password);
-if(compare){
-   req.json({message:"Logged in successfully"});
-}else{
-   req.json({message:"email or Password is wrong"});
-}
-
-  }else{
-   res.status(401).json({message : "User email or password wrong"});
+    res.json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "something went wrong" });
   }
-      
-   
-   } catch(error){
-      res.status(500).json({message:"something went wrong"});
-   
-   }
-   
-   })
+});
 
-app.listen(process.env.PORT||3001);
+app.post("/login", async function (req, res) {
+  try {
+    let connection = await mongoClient.connect(URL);
+    let db = connection.db(DB);
+
+    let user = await db.collection("User").findOne({ email : req.body.email });
+    if (user) {
+      let compare = await bcrypt.compare(req.body.password, user.password);
+    
+      if (compare) {
+        res.json({ message: "Logged in successfully" });
+      } else {
+        res.json({ message: "email or Password is wrong" });
+      }
+
+    } else {
+      res.status(401).json({ message: "User email or password wrong" });
+    }
+
+  } catch (error) {
+    res.status(500).json({ message: "something went wrong" });
+  }
+});
+
+app.listen(process.env.PORT || 3001);
